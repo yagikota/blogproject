d419c4b8b96affedfa45b061c121669f7a0a2321
八木洸太 <yagikota@mbp-2.local>
2021-04-15 10:39:22 +0900

    remove url admin

--- blogpost/urls.py
+++ blogpost/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from .views import BlogList, BlogDetail, BlogCreate, BlogDelete, BlogUpdate
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('list/', BlogList.as_view(), name='list'),
     path('detail/<int:pk>/', BlogDetail.as_view(), name='detail'),#primary key が指定されているid
     path('create/', BlogCreate.as_view(), name='create'),
